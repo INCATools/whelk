75cbe2214ebd0172dd89462a3ea5550651b8e85e
Jim Balhoff <jim@balhoff.org>
2020-07-04 13:46:02 -0400

    Output timings for additional reasoning scenarios.

--- src/main/scala/org/geneontology/whelk/Main.scala
+++ src/main/scala/org/geneontology/whelk/Main.scala
@@ -3,6 +3,7 @@ package org.geneontology.whelk
 import java.io.File
 import java.io.FileOutputStream
 import java.io.FileInputStream
+import java.util.UUID
 
 import org.semanticweb.owlapi.apibinding.OWLManager
 import org.semanticweb.owlapi.model.IRI
@@ -23,6 +24,35 @@ object Main extends App {
   val taxonomy = done.computeTaxonomy
   val stopTax = System.currentTimeMillis()
   println(s"Computed taxonomy in ${stopTax - startTax} ms")
+
+  val properties = done.hier.keys
+  val classes = done.inits.collect { case c: AtomicConcept => c }.take(100)
+  val expressions = for {
+    property <- properties
+    cls <- classes
+  } yield ExistentialRestriction(property, cls)
+  println(s"Start ${expressions.size} queries")
+  val startQueries = System.currentTimeMillis()
+  for (expression <- expressions) {
+    val cls = AtomicConcept(s"urn:uuid:${UUID.randomUUID.toString}")
+    val expressionDone = Reasoner.assert(Set(ConceptInclusion(cls, expression), ConceptInclusion(expression, cls)), done)
+    val subclasses = expressionDone.closureSubsBySuperclass.getOrElse(cls, Set.empty) + BuiltIn.Bottom
+    val minusEquivs = subclasses.diff(expressionDone.closureSubsBySubclass.getOrElse(cls, Set.empty))
+    val properSubclasses = minusEquivs.collect { case ac @ AtomicConcept(_) => ac }
+  }
+  val stopQueries = System.currentTimeMillis()
+  println(s"Queries done in: ${stopQueries - startQueries} ms")
+
+  val model = OWLManager.createOWLOntologyManager().loadOntology(IRI.create(new File(args(1))))
+  val modelAxioms = Bridge.ontologyToAxioms(model)
+  val modelConceptInclusions = modelAxioms.collect { case ci: ConceptInclusion => ci }
+  val startModel = System.currentTimeMillis
+  println("Start models")
+  for (i <- 0 to 300) {
+    val modelDone = Reasoner.assert(modelConceptInclusions, done)
+  }
+  val stopModel = System.currentTimeMillis
+  println(s"Models reasoned in: ${stopModel - startModel} ms")
 
   //val dumpStart = System.currentTimeMillis
 
